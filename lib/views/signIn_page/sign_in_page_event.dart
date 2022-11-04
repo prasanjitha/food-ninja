18dbb6548d53f5d7ac70386d04574ee6b329825f
prasanjitha <graphicdis126@gmail.com>
2022-11-04 23:43:22 +0530

    add admin home page notifications

--- lib/views/signIn_page/sign_in_page_event.dart
+++ lib/views/signIn_page/sign_in_page_event.dart
@@ -8,3 +8,9 @@ class LoginEvent extends SignINPageEvent {
   final String password;
   LoginEvent({required this.email, required this.password});
 }
+
+class NotificationsLoginEvent extends SignINPageEvent {
+  final String email;
+  final String password;
+  NotificationsLoginEvent({required this.email, required this.password});
+}
