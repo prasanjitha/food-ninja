185d154bdfdb1f631efae2776de15844dbe72f7c
prasanjitha <graphicdis126@gmail.com>
2022-11-25 15:35:01 +0530

    google api key

--- lib/views/main_home_page/main_home_page_view.dart
+++ lib/views/main_home_page/main_home_page_view.dart
@@ -122,6 +122,10 @@ class _MainHomePageViewState extends State<MainHomePageView> {
                     'Customer feedback api',
                     style: Theme.of(context).textTheme.headline4,
                   ),
+                  Text(
+                    'Google api key',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
                   Container(),
                   InkWell(
                     onTap: () {
